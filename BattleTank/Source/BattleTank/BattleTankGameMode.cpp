d573ee1ab01a22588e77d4a923f9849a18a0c2d9
Ben Tristem <github@bentristem.com>
2016-03-30 23:07:59 +0100

    BT04 Creating Our First Landscape

---
+++ BattleTank/Source/BattleTank/BattleTankGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "BattleTankGameMode.h"
+
+
+
+
---
+++ BattleTank/Source/BattleTank/BattleTankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleTankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
