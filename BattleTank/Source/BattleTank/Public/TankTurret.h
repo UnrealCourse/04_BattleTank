d27bbe5f6c76fad3a4a344ce91197abbd00999a7
Ben Tristem <github@bentristem.com>
2016-06-22 21:30:38 +0100

    BT47 Working Round Awkward Bugs.cmproj

--- BattleTank/Source/BattleTank/Public/TankTurret.h
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -8,7 +8,7 @@
 /**
  * 
  */
-UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+UCLASS(meta = (BlueprintSpawnableComponent)) //, hidecategories = ("Collision"))
 class BATTLETANK_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
