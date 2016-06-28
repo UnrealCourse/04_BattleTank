a7cc21865667d6f02052a62e69bba392e85e99ad
Ben Tristem <github@bentristem.com>
2016-06-28 20:29:53 +0100

    BT54 ApplyForceAtLocation() in Action.cmproj

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -12,4 +12,8 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
 
 	// TODO clamp actual throttle value so player can't over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);	
 }
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -18,5 +18,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
-	
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g accelleration
 };
