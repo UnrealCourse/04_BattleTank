e957473f8e25bd8b7ec7abb4e6441ac7fef8e3a8
Ben Tristem <github@bentristem.com>
2016-06-29 20:18:36 +0100

    BT56 Fly-by-Wire Control System.cmproj

---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
