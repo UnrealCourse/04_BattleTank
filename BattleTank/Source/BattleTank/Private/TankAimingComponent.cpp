62d6fdc39717abc707ba12dd5b0f91d37edd8351
Ben Tristem <github@bentristem.com>
2016-07-24 21:07:29 +0100

    BT86 Making an AutoMortar.cmproj

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -104,11 +104,11 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 
 	// Always yaw the shortest way
 	Barrel->Elevate(DeltaRotator.Pitch);
-	if (DeltaRotator.Yaw < 180)
+	if (FMath::Abs(DeltaRotator.Yaw) < 180)
 	{
 		Turret->Rotate(DeltaRotator.Yaw);
 	}
-	else
+	else // Avoid going the long-way round
 	{
 		Turret->Rotate(-DeltaRotator.Yaw);
 	}
